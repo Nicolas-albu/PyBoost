43014c08a6011947df1a2c497b10977cbfb56489
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-16 03:12:03 -0300

    :hammer_and_wrench: Added the __all__ and moved the all constants of utils.py

--- pyboost/core/__init__.py
+++ pyboost/core/__init__.py
@@ -1,3 +1,24 @@
-from .generator import Generator
-from .utils import (generate_pyboost_json, get_current_path, get_path_name,
-                    get_testing_path, processing_directory)
+from pathlib import Path
+
+from .beautify import BeautifyConsole
+from .pyboost import PyBoost
+
+__NAME_CONFIG_FILE__ = "pyboost.json"
+
+# Gets the name of the folder where PyBoost is running.
+__PATH_NAME__: str = Path().cwd().name
+
+# Gets the current path directory.
+__CURRENT_PATH__: Path = Path().cwd()
+
+# Gets the path for debug the PyBoostCLI.
+__DEBUG_PATH__: Path = __CURRENT_PATH__ / "scripts" / "debug"
+
+__all__ = [
+    "PyBoost",
+    "__PATH_NAME__",
+    "__DEBUG_PATH__",
+    "BeautifyConsole",
+    "__CURRENT_PATH__",
+    "__NAME_CONFIG_FILE__",
+]
