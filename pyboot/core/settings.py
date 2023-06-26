a20d1a3b09935299299a52c5639135231d7140df
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-26 19:07:49 -0300

    :star: Added settings.py file

---
+++ pyboot/core/settings.py
@@ -0,0 +1,22 @@
+from os.path import abspath, dirname
+from pathlib import Path
+
+__NAME_CONFIG_FILE__ = 'pyboot.toml'
+
+# Gets the current path directory.
+__CURRENT_PATH__: Path = Path().cwd()
+
+# Gets the name of the folder where PyBoot is running.
+__PATH_NAME__: str = Path().cwd().name
+
+__BASE_DIR__ = Path(dirname(abspath(__file__))).parent.parent
+
+__SCRIPTS_PATH__: Path = __BASE_DIR__ / 'scripts'
+
+__SECRETS_CONF_DJANGO__: Path = __SCRIPTS_PATH__ / 'secrets_conf_django.yaml'
+
+__SETTINGS_CONF_DJANGO__: Path = __SCRIPTS_PATH__ / 'settings_conf_django.yaml'
+
+__SETTINGS_DJANGO__: Path = __SCRIPTS_PATH__ / 'settings_django.py'
+
+__GITIGNORE_DJANGO__: Path = __SCRIPTS_PATH__ / 'conf_django.gitignore'
