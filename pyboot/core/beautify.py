a70812f0789922066f4c3815195c48d377721fba
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-26 19:10:13 -0300

    :star: Added the file to beautify the terminal of CLI

--- pyboot/core/beautify.py
+++ pyboot/core/beautify.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from rich.console import Console
 from rich.progress import Progress
 
-from . import __NAME_CONFIG_FILE__
+from .settings import __NAME_CONFIG_FILE__
 
 
 class BeautifyConsole:
@@ -17,18 +17,18 @@ class BeautifyConsole:
         pyboost_config = (directory / __NAME_CONFIG_FILE__).read_text()
 
         # printing results
-        self.console.print_json(pyboost_config)
+        self.console.print(pyboost_config)
         self.console.print(
-            f"\n[bold green]{name_project!r} configured![/bold green] :rocket:"
+            f'\n[bold green]{name_project!r} configured![/bold green] :rocket:'
         )
 
     def add_progressbar(self) -> None:
         with Progress() as progress:
             task_generate_pyboost_json = progress.add_task(
-                "[bold yellow]Generate pyboost.json", total=100
+                '[bold yellow]Generate pyboost.json', total=100
             )
             task_create_files = progress.add_task(
-                "[bold red]Creating files[/bold red]", total=100
+                '[bold red]Creating files[/bold red]', total=100
             )
 
             while not progress.finished:
