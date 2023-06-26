2937e1ba3e875281b62e5a08b874a2caaca395b4
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-26 19:15:31 -0300

    :gear: Updated __init__.py

--- pyboot/core/__init__.py
+++ pyboot/core/__init__.py
@@ -1,24 +1,14 @@
-from pathlib import Path
-
 from .beautify import BeautifyConsole
 from .pyboot import PyBoot
-
-__NAME_CONFIG_FILE__ = "pyboot.json"
-
-# Gets the name of the folder where PyBoot is running.
-__PATH_NAME__: str = Path().cwd().name
-
-# Gets the current path directory.
-__CURRENT_PATH__: Path = Path().cwd()
-
-# Gets the path for debug the PyBootCLI.
-__DEBUG_PATH__: Path = __CURRENT_PATH__ / "scripts" / "debug"
+from .settings import __CURRENT_PATH__, __NAME_CONFIG_FILE__, __PATH_NAME__
 
 __all__ = [
-    "PyBoot",
-    "__PATH_NAME__",
-    "__DEBUG_PATH__",
-    "BeautifyConsole",
-    "__CURRENT_PATH__",
-    "__NAME_CONFIG_FILE__",
+    # pyboot
+    'PyBoot',
+    # settings
+    '__PATH_NAME__',
+    '__CURRENT_PATH__',
+    '__NAME_CONFIG_FILE__',
+    # beautify
+    'BeautifyConsole',
 ]
