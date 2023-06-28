e3a85b75069bd80ef93bf8bbeefe144f2b5eaa99
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-27 21:43:01 -0300

    :hammer_and_wrench: Added __REQUIREMENTS__

--- pyboot/core/settings.py
+++ pyboot/core/settings.py
@@ -11,12 +11,22 @@ __PATH_NAME__: str = Path().cwd().name
 
 __BASE_DIR__ = Path(dirname(abspath(__file__))).parent.parent
 
-__SCRIPTS_PATH__: Path = __BASE_DIR__ / 'scripts' / 'django_blank'
+__SCRIPTS_PATH__: Path = __BASE_DIR__ / 'scripts'
 
-__SECRETS_CONF_DJANGO__: Path = __SCRIPTS_PATH__ / 'secrets_conf_django.yaml'
+__TEMPLATE_DJANGO_BLANK__: Path = __SCRIPTS_PATH__ / 'django_blank'
 
-__SETTINGS_CONF_DJANGO__: Path = __SCRIPTS_PATH__ / 'settings_conf_django.yaml'
+__SETTINGS_DJANGO__: Path = __TEMPLATE_DJANGO_BLANK__ / 'settings_django.py'
 
-__SETTINGS_DJANGO__: Path = __SCRIPTS_PATH__ / 'settings_django.py'
+__REQUIREMENTS__: Path = __TEMPLATE_DJANGO_BLANK__ / 'requirements.txt'
 
-__GITIGNORE_DJANGO__: Path = __SCRIPTS_PATH__ / 'conf_django.gitignore'
+__SECRETS_CONF_DJANGO__: Path = (
+    __TEMPLATE_DJANGO_BLANK__ / 'secrets_conf_django.yaml'
+)
+
+__SETTINGS_CONF_DJANGO__: Path = (
+    __TEMPLATE_DJANGO_BLANK__ / 'settings_conf_django.yaml'
+)
+
+__GITIGNORE_DJANGO__: Path = (
+    __TEMPLATE_DJANGO_BLANK__ / 'conf_django.gitignore'
+)
