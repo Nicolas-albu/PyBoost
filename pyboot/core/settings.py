4c7406a4b21fa1d97b1d76464a848f8d468331b7
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-28 02:54:27 -0300

    :hammer_and_wrench: Added Template class and defined template_django_blank object

--- pyboot/core/settings.py
+++ pyboot/core/settings.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from os.path import abspath, dirname
 from pathlib import Path
 
@@ -13,20 +14,49 @@ __BASE_DIR__ = Path(dirname(abspath(__file__))).parent.parent
 
 __SCRIPTS_PATH__: Path = __BASE_DIR__ / 'scripts'
 
-__TEMPLATE_DJANGO_BLANK__: Path = __SCRIPTS_PATH__ / 'django_blank'
+__ENVIRONMENT_STAGES__: tuple = 'development', 'testing', 'production'
 
-__SETTINGS_DJANGO__: Path = __TEMPLATE_DJANGO_BLANK__ / 'settings_django.py'
 
-__REQUIREMENTS__: Path = __TEMPLATE_DJANGO_BLANK__ / 'requirements.txt'
+@dataclass(slots=True, eq=False, repr=False)
+class Template:
+    """Class that contains all paths to templates."""
 
-__SECRETS_CONF_DJANGO__: Path = (
-    __TEMPLATE_DJANGO_BLANK__ / 'secrets_conf_django.yaml'
-)
+    template_path: Path
+    requirements: Path | str
+    settings_django: Path | str
+    gitignore_django: Path | str
+    secrets_dynf_django: Path | str
+    settings_dynf_django: Path | str
 
-__SETTINGS_CONF_DJANGO__: Path = (
-    __TEMPLATE_DJANGO_BLANK__ / 'settings_conf_django.yaml'
-)
+    def __post_init__(self):
+        self.requirements = self.template_path / self.requirements
+        self.settings_django = self.template_path / self.settings_django
+        self.gitignore_django = self.template_path / self.gitignore_django
+        self.secrets_dynf_django = (
+            self.template_path / self.secrets_dynf_django
+        )
+        self.settings_dynf_django = (
+            self.template_path / self.settings_dynf_django
+        )
 
-__GITIGNORE_DJANGO__: Path = (
-    __TEMPLATE_DJANGO_BLANK__ / 'conf_django.gitignore'
+    def __iter__(self):
+        yield from (
+            self.secrets_dynf_django,
+            self.settings_django,
+            self.gitignore_django,
+            self.settings_dynf_django,
+            self.requirements,
+        )
+
+
+# Configurations of template django-blank
+__DJANGO_BLANK__: Path = __SCRIPTS_PATH__ / 'django_blank'
+
+template_django_blank = Template(
+    template_path=__DJANGO_BLANK__,
+    requirements='requirements.txt',
+    settings_django='settings_django.py',
+    gitignore_django='cfg_django.gitignore',
+    secrets_dynf_django='secrets_dynf_django.yaml',
+    settings_dynf_django='settings_dynf_django.yaml',
 )
