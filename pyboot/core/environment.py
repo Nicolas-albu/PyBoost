ad7bd28b0a78e0622cf7aabe13a416747c1f1f36
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-30 11:53:56 -0300

    :hammer_and_wrench: __venv_path gets the venv in the project path if it exists, otherwise creates it

--- pyboot/core/environment.py
+++ pyboot/core/environment.py
@@ -4,48 +4,58 @@ import subprocess
 import venv
 from pathlib import Path
 
+from .settings import __DEFAULT_VENV_NAME__, __VENV_NAMES__
+
 
 class Environment:
-    __slots__ = ['__project_path', 'system', '__venv_path']
+    __slots__ = ['__project_path', '__venv_path', 'system']
 
     def __init__(self, *, project_path: Path):
         self.__project_path = project_path
-        self.__venv_path = self.__project_path / ".venv"
         self.system = platform.system()
 
-        self.__create_venv_path()
+        venv_name: str | None = self.get_venv()
+        self.__venv_path = self.__project_path / (
+            venv_name if venv_name else __DEFAULT_VENV_NAME__
+        )
+        if not venv_name:
+            self.__create_venv_path()
 
     @property
     def venv_pip(self) -> Path:
-        if self.system == "Windows":
-            return self.__venv_path / "Scripts" / "pip.exe"
+        if self.system == 'Windows':
+            return self.__venv_path / 'Scripts' / 'pip.exe'
         return self.__venv_path / 'bin' / 'pip'
 
     @property
     def venv_python(self) -> Path:
-        if self.system == "Windows":
-            return self.__venv_path / "Scripts" / "python.exe"
-        return self.__venv_path / "bin" / "python"
+        if self.system == 'Windows':
+            return self.__venv_path / 'Scripts' / 'python.exe'
+        return self.__venv_path / 'bin' / 'python'
 
     @property
     def django_admin(self) -> Path:
-        if self.system == "Windows":
-            return self.__venv_path / "Scripts" / "django-admin.exe"
-        return self.__venv_path / "bin" / "django-admin"
+        if self.system == 'Windows':
+            return self.__venv_path / 'Scripts' / 'django-admin.exe'
+        return self.__venv_path / 'bin' / 'django-admin'
 
     def __create_venv_path(self) -> None:
         venv.create(self.__venv_path, with_pip=True)
         subprocess.run(
-            [self.venv_pip, "install", "-U", "pip", "setuptools"],
+            [self.venv_pip, 'install', '-U', 'pip', 'setuptools'],
             check=True,
             stdout=open(os.devnull, 'w'),
         )
 
+    def get_venv(self) -> str | None:
+        for venv_name in __VENV_NAMES__:
+            if (self.__project_path / venv_name).exists():
+                return venv_name
+
+        return None
+
     @staticmethod
     def execute(command: str, /) -> None:
-        # command = command.replace('$PYTHON_VENV', str(self.venv_python))
-        # args_command = command.split()
-
         subprocess.run(
             [command],
             check=True,
