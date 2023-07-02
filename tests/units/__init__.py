01402599813ad3a7fc4e158bb48e85a023ea3f76
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-02 02:40:16 -0300

    :wrench: fixtures_to_debug_folder method added

--- tests/units/__init__.py
+++ tests/units/__init__.py
@@ -19,6 +19,20 @@ def back_before(
         file.unlink(missing_ok=True)
 
 
+def fixtures_to_debug_folder(
+    *,
+    fixtures_filename: str,
+    debug_filename: str,
+) -> Path:
+    fixture_file = fixtures_path / fixtures_filename
+    debug_file = debug_path / debug_filename
+    debug_file.touch()
+
+    shutil.copy2(fixture_file, debug_file)
+
+    return debug_file
+
+
 __all__ = [
     'fixtures_path',
     'units_path',
