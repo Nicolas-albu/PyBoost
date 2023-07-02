f13443d33052aad308a8b7b4f8a8c9d98afd0bf6
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-02 00:42:10 -0300

    :hammer_and_wrench: test python version file creation added

--- tests/units/test_builder.py
+++ tests/units/test_builder.py
@@ -19,3 +19,14 @@ def test_generation_tokens_with_maxsize_100(test_builder: Builder):
     token = ''.join(tuple(_token))
 
     assert re.search(pattern, token) is None
+
+
+def test_python_version_file_creation(test_builder: Builder):
+    python_version: str = '3.9.1'
+
+    test_builder.add_python_version_file(python_version)
+
+    python_version_file = debug_path / '.python-version'
+
+    assert python_version_file.exists()
+    assert python_version_file.read_text() == python_version
