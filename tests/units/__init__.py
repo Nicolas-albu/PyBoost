0abee2def151e7d263c2ac5a15f76acdd248617d
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-02 01:28:30 -0300

    :hammer_and_wrench: back_before method added

--- tests/units/__init__.py
+++ tests/units/__init__.py
@@ -1,9 +1,27 @@
+import shutil
 from pathlib import Path
+from typing import Optional
 
+fixtures_path = Path().cwd() / 'tests' / 'fixtures'
 units_path = Path().cwd() / 'tests' / 'units'
 debug_path = units_path / 'debug'
 
+
+def back_before(
+    *,
+    folder: Optional[Path] = None,
+    file: Optional[Path] = None,
+) -> None:
+    if folder:
+        shutil.rmtree(folder)
+
+    if file:
+        file.unlink(missing_ok=True)
+
+
 __all__ = [
+    'fixtures_path',
     'units_path',
     'debug_path',
+    'back_before',
 ]
