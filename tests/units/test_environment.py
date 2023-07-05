180f8da63a109b0faa539ab10c37446d5dd508e3
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-05 02:47:46 -0300

    :wrench: The use of test_environment updated to environment fixture

--- tests/units/test_environment.py
+++ tests/units/test_environment.py
@@ -1,36 +1,29 @@
-import pytest
-
 from pyboot.core.environment import Environment
 
 from . import back_before, debug_path
 
 
-@pytest.fixture
-def test_environment():
-    return Environment(project_path=debug_path)
-
-
-def test_get_venv(test_environment: Environment):
+def test_get_venv(environment: Environment):
     venv_path = debug_path / '.venv'
     venv_path.mkdir(exist_ok=True)
 
-    assert test_environment.get_venv_name() == '.venv'
+    assert environment.get_venv_name() == '.venv'
     back_before(folder=venv_path)
 
     venv_path = debug_path / 'venv'
     venv_path.mkdir(exist_ok=True)
 
-    assert test_environment.get_venv_name() == 'venv'
+    assert environment.get_venv_name() == 'venv'
     back_before(folder=venv_path)
 
 
-def test_create_venv(test_environment: Environment):
-    test_environment.create_venv()
+def test_create_venv(environment: Environment):
+    environment.create_venv()
     venv_path = debug_path / '.venv'
 
-    assert test_environment.venv_name == '.venv'
+    assert environment.venv_name == '.venv'
     assert venv_path.exists()
-    assert test_environment.venv_pip.exists()
-    assert test_environment.venv_python.exists()
+    assert environment.venv_pip.exists()
+    assert environment.venv_python.exists()
 
     back_before(folder=venv_path)
