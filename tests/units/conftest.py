cec5671f979c8af17bf1261b1b7d4307c658706e
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-06 22:00:22 -0300

    :wrench: Update the name of debug_path to out_path

--- tests/units/conftest.py
+++ tests/units/conftest.py
@@ -3,14 +3,14 @@ import pytest
 from pyboot.core.builder import Builder
 from pyboot.core.environment import Environment
 
-from . import debug_path
+from . import out_path
 
 
 @pytest.fixture
 def environment():
-    yield Environment(project_path=debug_path)
+    yield Environment(project_path=out_path)
 
 
 @pytest.fixture
 def builder():
-    yield Builder(project_path=debug_path)
+    yield Builder(project_path=out_path)
