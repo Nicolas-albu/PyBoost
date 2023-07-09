6974d1e78fd3073435103674616dcc02c8a8c535
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-09 11:52:08 -0300

    :hammer_and_wrench: feat: __TEMPLATES_NAME__ constant added

--- pyboot/core/settings.py
+++ pyboot/core/settings.py
@@ -18,3 +18,7 @@ __BASE_DIR__ = Path(dirname(abspath(__file__))).parent.parent
 __TEMPLATES_PATH__: Path = __BASE_DIR__ / 'templates'
 
 __ENVIRONMENT_STAGES__: tuple = 'development', 'testing', 'production'
+
+__TEMPLATES_NAME__ = [
+    'django4.2-blank',
+]
