da005e3dc18b76d5e0a9d3d69230aeb9042e1679
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-07-09 23:48:45 -0300

    :construction: refactor: added handling for boolean value situations

--- pyboot/core/reader.py
+++ pyboot/core/reader.py
@@ -6,4 +6,8 @@ def toml_dump(obj: dict, file: TextIOWrapper) -> None:
         raise TypeError('You can only dump an object to a file descriptor')
 
     for key, value in obj.items():
-        file.write(f'{key} = "{value}"\n')
+        if isinstance(value, bool):
+            value = str(value).lower()
+            file.write(f'{key} = {value}\n')
+        else:
+            file.write(f'{key} = "{value}"\n')
